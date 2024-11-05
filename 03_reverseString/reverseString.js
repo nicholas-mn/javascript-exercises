652d7029517072d23c4dc1a783d3a4c46e344631
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2024-11-05 23:38:07 +0100

    Complete Exercise 03 - Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let output = ""
 
+    for (i = string.length; i > 0; i--) {
+        const stringLength = string.length;
+
+        output += string.charAt(i - 1);
+    }
+
+    return output;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
