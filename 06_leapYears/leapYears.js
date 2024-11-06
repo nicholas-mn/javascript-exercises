35c351b243639b3879f916f0caeeeac07ade7858
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2024-11-06 19:31:02 +0100

    Complete Exercise 06 - leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {return true}
+        return false
+    } 
+
+    if (year % 4 === 0) {
+        return true
+    }
+    
+    return false
 };
+
+console.log(leapYears(2000))
 
 // Do not edit below this line
 module.exports = leapYears;
