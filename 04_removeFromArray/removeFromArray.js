f4a6410d2c3b1fdb1e930f794fe54ca5192f20ea
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2024-11-06 17:44:48 +0100

    Exercise 04 - removeFromArray, pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arrayRemove) {
+    
+    newArray = []
+
+    for (const x of array) {
+        
+        if (x === arrayRemove) {
+            continue
+        } else {
+        
+        newArray.push(x)
+        }
+    }
+
+    return newArray
 };
 
 // Do not edit below this line
